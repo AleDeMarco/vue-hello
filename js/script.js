2c54c48d00c60280278a88968277b4982702d5f5
Alessandro De Marco <lookatde.lad@gmail.com>
2021-01-25 16:42:14 +0100

    Esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    text: 'Diventa un full stack web dev!',
+    img: 'https://www.boolean.careers/images/misc/logo.png',
+    alt: 'Boolean'
+  }
+});
